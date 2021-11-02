fb98f591884b73190fe32af0cfb4c427e560def6
dack10 <2155934560101@ingenieria.usac.edu.gt>
2021-11-01 23:50:02 -0600

    fecha y cantidad

--- appFlask/tutorial.py
+++ appFlask/tutorial.py
@@ -1,7 +1,12 @@
+from enum import auto
+from os import path
+from tkinter.filedialog import Open
 from flask import Flask,request
 from flask_cors import CORS
+from flask_cors.core import serialize_option
 import requests
 import xml.etree.ElementTree as xml
+import re
 
 app = Flask(__name__) 
 #CREAMOS UNA INSTANCIA DE ESTA CLASE EL ARGUMENTO QUE SE LE PASA ES NAME POR QUE LA APP 
@@ -29,10 +34,76 @@ def procesar():
 
 @app.route("/proceso", methods=['POST'])
 def proceso():    
-    texto = request.data.decode('utf-8')
-    print(texto)
-    file=open("prueba.xml",'w')
-    file.write(texto)
+    #texto = request.data.decode('utf-8')
+    #print(texto)
+    #file=open("prueba.xml",'w')
+    #file.write(texto)
+    #file.close()
+    xmll=xml.parse('prueba.xml')
+    xml2=xml.parse('prueba.xml')
+    root1=xmll.getroot()
+    root2=xml2.getroot()
+    contador=0
+    for dte in root1.findall('DTE'):
+        for tiempo in dte.findall('TIEMPO'):
+            fecha=tiempo.text
+            expresion=re.search(r"[0-9]+/[0-9]+/[0-9]+",str(fecha))
+            exp=expresion.group()
+            print("elemento de lista "+exp)
+            for dte2 in root2.findall('DTE'):
+                for tiemp in dte2.findall('TIEMPO'):
+                    fech=tiemp.text
+                    expresion2=re.search(r"[0-9]+/[0-9]+/[0-9]+",str(fech))
+                    expresion3=expresion2.group()
+                    print("elemento de la otra lista para comparar "+expresion3)
+                    if expresion3==exp:
+                        contador=contador+1
+        print(contador)
+        contador=0  
+    
+    raiz=xml.Element('LISTAAUTORIZACIONES')
+    #for num in range(0,3):
+        #autorizaciones=xml.SubElement(raiz,'AUTORIZACION')
+    exp2=""
+    for dte in root1.findall('DTE'):
+        for tiempo in dte.findall('TIEMPO'):
+            fecha=tiempo.text
+            expresion=re.search(r"[0-9]+/[0-9]+/[0-9]+",str(fecha))
+            exp=expresion.group()
+            if exp!=exp2: 
+                print("elemento de lista "+exp)
+                for dte2 in root2.findall('DTE'):
+                    for tiemp in dte2.findall('TIEMPO'):
+                        fech=tiemp.text
+                        expresion2=re.search(r"[0-9]+/[0-9]+/[0-9]+",str(fech))
+                        expresion3=expresion2.group()
+                        print("elemento de la otra lista para comparar "+expresion3)
+                        if expresion3==exp:
+                            contador=contador+1
+                print("la fecha "+str(exp)+" "+"esta "+str(contador)+" veces")
+                autorizaciones=xml.SubElement(raiz,'AUTORIZACION')
+                fecha=xml.SubElement(autorizaciones,'FECHA')
+                fecha.text=str(exp)
+                facturas=xml.SubElement(autorizaciones,'FACTURAS_RECIBIDAS')
+                facturas.text=str(contador)
+                print(str(contador))
+                contador=0
+            exp2=exp
+                  
+    
+    myraiz=xml.ElementTree(raiz)
+    myraiz.write('autorizaciones.xml')
+    file=open('autorizaciones.xml')
+    return 0
+
+
+    
+
+   
+    
+    
+
+
     
 
 @app.route("/registro",methods=['POST'])
