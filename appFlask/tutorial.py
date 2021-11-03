057b3708464e5b00f8594ad5aee85af63b22d31a
dack10 <2155934560101@ingenieria.usac.edu.gt>
2021-11-03 00:20:16 -0600

    AMBOS NIT

--- appFlask/tutorial.py
+++ appFlask/tutorial.py
@@ -1,5 +1,5 @@
 from enum import auto
-from os import path
+from os import error, path
 from tkinter.filedialog import Open
 from flask import Flask,request
 from flask_cors import CORS
@@ -45,6 +45,7 @@ def proceso():
     root2=xml2.getroot()
     contador=0
     errorNitEmisor=0
+    errorNitReceptor=0
     """for dte in root1.findall('DTE'):
         for tiempo in dte.findall('TIEMPO'):
             fecha=tiempo.text
@@ -90,34 +91,103 @@ def proceso():
 
                 errores=xml.SubElement(autorizaciones,'ERRORES')#creacion de etiqueta errores
                 emisor=xml.SubElement(errores,'NIT_EMISOR')#creacion de etiqueta nit emisor dentro de errores
-                nitEmisor= dte.find('NIT_EMISOR').text
-                print(str(nitEmisor))
-                longitud=len(nitEmisor)
-                expresionNitEmisor=re.search(r"[0-9]+",str(nitEmisor))
-                tieneLetras=re.search(r"\D+",str(nitEmisor))
-                print(tieneLetras.group())
-                for num in range(1,3):
-                    if num==1:
-                        for emi in root1.findall('DTE'):
-                            for emi2 in emi.findall("NIT_EMISOR"):
-                                dteee=emi2.text
-                                longitud=len(dteee)
-                                expresionNitEmisor=re.search(r"[0-9]+",str(dteee))
-                                tieneLetras=re.search(r"\D+",str(dteee))
-                                if len(expresionNitEmisor.group())>20 or len(tieneLetras.group())>0:
-                                    errorNitEmisor=errorNitEmisor+1
-                        errorn=xml.SubElement(errores,'NIT_EMISOR')
-                        errorn.text=str(errorNitEmisor)
-                errorNitEmisor=0       
-
+                #metodo de encontrar errores de nit emisor
+                
+                for m in root1.findall('DTE'):
+                    for nn in m.findall('TIEMPO'):
+                        fAcomparar=nn.text
+                        fExpresion=re.search(r"[0-9]+/[0-9]+/[0-9]+",str(fAcomparar))
+                        fgroup=fExpresion.group()
+                        if exp==fgroup:
+                            nit=m.find('NIT_EMISOR').text
+                            nitSinEspacios=nit.lstrip()
+                            nitsinespacios2=nitSinEspacios.rstrip()
+                            print("este es el nit a probar "+str(nitsinespacios2))
+                            longitud=len(nitsinespacios2)#OBTENGO LA LONGITUD DEL NIT
+                            print("la longitud de este nit es "+str(longitud))
+                            letras=re.search(r"\D+",str(nitsinespacios2))#ver si tiene letras
+                            print("estas son las letras"+str(letras))
+                            if longitud>20 or letras!=None:
+                                errorNitEmisor=errorNitEmisor+1
+                                
+                            
+                            expresionNitE=re.findall(r"[0-9]+",str(nit))#obtener solo los numero del nit
+                            eNe=""
+                            for n in range(0,len(expresionNitE)):
+                                t=expresionNitE[n]
+                                eNe=str(eNe)+str(t)
+                            #print(eNe)#IMPRIMO EL NIT EN SOLO NUMEROS
+                emisor.text=str(errorNitEmisor)          
+                print("cantidad de errores de nit emisor de la fecha "+str(exp)+" " +"es "+str(errorNitEmisor))
+                #-------------------------------------------------------------------------------------------
+                #nitEmisor= dte.find('NIT_EMISOR').text #obtener el texto nit emisorde la etiqueta
+                #print(str(nitEmisor))
+                #longitud=len(nitEmisor) #longitur del nit obtenido
+                #print(longitud)
+                #expresionNitEmisor=re.findall(r"[0-9]+",str(nitEmisor))#obtener solo los numero del nit
+                #eNe=""
+                #for n in range(0,len(expresionNitEmisor)):
+                #    t=expresionNitEmisor[n]
+                #    eNe=str(eNe)+str(t)
+                #print(eNe)
+                #tieneLetras=re.search(r"\D+",str(nitEmisor))
+                #print(tieneLetras.group())
+                #for num in range(1,3):
+                #    if num==1:
+                #        for emi in root1.findall('DTE'):
+                #            for emi2 in emi.findall("NIT_EMISOR"):
+                #                dteee=emi2.text
+                #                longitud=len(dteee)
+                #                expresionNitEmisor=re.search(r"[0-9]+",str(dteee))
+                #                tieneLetras=re.search(r"\D+",str(dteee))
+                #                if len(expresionNitEmisor.group())>20 or len(tieneLetras.group())>0:
+                #                    errorNitEmisor=errorNitEmisor+1
+                #        errorn=xml.SubElement(errores,'NIT_EMISOR')
+                #        errorn.text=str(errorNitEmisor)
+                   
+                #-----------------------------------------------------------------------------------------------
                 receptor=xml.SubElement(errores,'NIT_RECEPTOR')
+                #metodo de encontrar errores de nit emisor
+                
+                for m in root1.findall('DTE'):
+                    for nn in m.findall('TIEMPO'):
+                        fAcomparar=nn.text
+                        fExpresion=re.search(r"[0-9]+/[0-9]+/[0-9]+",str(fAcomparar))
+                        fgroup=fExpresion.group()
+                        if exp==fgroup:
+                            nit=m.find('NIT_RECEPTOR').text
+                            nitSinEspacios=nit.lstrip()
+                            nitsinespacios2=nitSinEspacios.rstrip()
+                            print("este es el nit a probar "+str(nitsinespacios2))
+                            longitud=len(nitsinespacios2)#OBTENGO LA LONGITUD DEL NIT
+                            print("la longitud de este nit es "+str(longitud))
+                            letras=re.search(r"\D+",str(nitsinespacios2))#ver si tiene letras
+                            print("estas son las letras"+str(letras))
+                            if longitud>20 or letras!=None:
+                                errorNitReceptor=errorNitReceptor+1
+                                
+                            
+                            expresionNitE=re.findall(r"[0-9]+",str(nit))#obtener solo los numero del nit
+                            eNe=""
+                            for n in range(0,len(expresionNitE)):
+                                t=expresionNitE[n]
+                                eNe=str(eNe)+str(t)
+                            #print(eNe)#IMPRIMO EL NIT EN SOLO NUMEROS
+                receptor.text=str(errorNitReceptor)          
+                print("cantidad de errores de nit emisor de la fecha "+str(exp)+" " +"es "+str(errorNitReceptor))
+                #---------------------------------------------------------------------------------------------------
+            
                 iva=xml.SubElement(errores,'IVA')
                 total=xml.SubElement(errores,'TOTAL')
                 duplicada=xml.SubElement(errores,'REFERENCIA_DUPLICADA')
                 print(str(contador))
                 contador=0
+                errorNitEmisor=0
+                errorNitReceptor=0
+                
             exp2=exp
-    errores=xml.SubElement(autorizaciones,'ERRORES')
+            
+    """errores=xml.SubElement(autorizaciones,'ERRORES')
     for emi in root1.findall('DTE'):
         for emi2 in emi.findall("NIT_EMISOR"):
             dteee=emi2.text
@@ -128,23 +198,16 @@ def proceso():
                 errorNitEmisor=errorNitEmisor+1
         errorn=xml.SubElement(errores,'NIT_EMISOR')
         errorn.text=str(errorNitEmisor)
-    
-                  
-    print("error "+str(errorNitEmisor))
+    """      
     myraiz=xml.ElementTree(raiz)
     myraiz.write('autorizaciones.xml')
     file=open('autorizaciones.xml')
-    return 0
+    file.close()
+    algo={
+        'hola':"hola"
+    }
+    return algo
 
-
-    
-
-   
-    
-    
-
-
-    
 
 @app.route("/registro",methods=['POST'])
 def registro():
